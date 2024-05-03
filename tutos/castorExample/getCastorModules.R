4cba6c41c756ea12f829576e7b050d14528c97b1
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2024-05-03 11:26:30 -0700

    enable db download in getCastorModules fun

--- tutos/castorExample/getCastorModules.R
+++ tutos/castorExample/getCastorModules.R
@@ -4,23 +4,28 @@
 #' @param name passed to [`setupModules()`]
 #' @param paths passed to [`setupModules()`]
 #' @param overwrite should modules be redownloaded and overwritten?
+#' @param dbURL optional. A URL to a SQLite database to be used locally
+#' @param dbPath optional. A folder path to place the downloaded SQLite database.
+#'   Defaults to `./`.
 #'
-#' @return a named vector of module folder paths
+#' @return a named list of module folder paths and, if dbURL is provided,
+#'   the SQLite database file path
 #' @export
 #'
-#' @examples
-getCastorModules <- function(modules = c("dataCastor", 
-                                         "growingStockCastor", 
-                                         "blockingCastor", 
-                                         "forestryCastor", 
-                                         "roadCastor"), 
-                             paths = NULL, 
-                             overwrite = FALSE) {
+#' @importsFrom reproducible prepInputs
+getCastorModulesAndDB <- function(modules = c("dataCastor",
+                                              "growingStockCastor",
+                                              "blockingCastor",
+                                              "forestryCastor",
+                                              "roadCastor"),
+                                  paths = NULL,
+                                  overwrite = FALSE,
+                                  dbURL, dbPath) {
   if (is.null(paths)) {
     stop("Provide 'paths'")
   }
   
-  finalModPaths <- normalizePath(file.path(paths$modulePath, modules, paste0(modules, ".R")), winslash = "/", 
+  finalModPaths <- normalizePath(file.path(paths$modulePath, modules, paste0(modules, ".R")), winslash = "/",
                                  mustWork = FALSE)
   if (any(!file.exists(finalModPaths)) | overwrite) {
     message("Could not find all modules locally. Downloading from GitHub")
@@ -33,8 +38,8 @@ getCastorModules <- function(modules = c("dataCastor",
   
   modFiles <- normalizePath(list.files(file.path(paths$modulePath, "castor/R/SpaDES-modules", modules),
                                        recursive = TRUE, full.names = TRUE), winslash = "/", mustWork = FALSE)
-  modFilesNew <- sub(normalizePath(file.path(paths$modulePath, "castor/R/SpaDES-modules"), winslash = "/", mustWork = FALSE), 
-                     normalizePath(paths$modulePath, winslash = "/", mustWork = FALSE), 
+  modFilesNew <- sub(normalizePath(file.path(paths$modulePath, "castor/R/SpaDES-modules"), winslash = "/", mustWork = FALSE),
+                     normalizePath(paths$modulePath, winslash = "/", mustWork = FALSE),
                      modFiles, fixed =TRUE)
   
   invisible(lapply(unique(dirname(modFilesNew)), dir.create, recursive = TRUE, showWarnings = FALSE))
@@ -43,5 +48,21 @@ getCastorModules <- function(modules = c("dataCastor",
   
   out <- finalModPaths
   names(out) <- modules
-  return(out)
+  
+  ## get db if
+  if (!missing(dbURL)) {
+    if (!requireNamespace("reproducible")) {
+      stop("Install 'reproducible' package to download a SQLite database")
+    }
+    
+    if (missing(dbPath)) {
+      dbPath <- normalizePath(getwd(), winslash = "/")
+    }
+    
+    repOut <- reproducible::prepInputs(url = dbURL,
+                                       destinationPath = dbPath,
+                                       fun = NA)
+  }
+  
+  return(list(modules = out, dbFilePath = repOut$targetFilePath))
 }
