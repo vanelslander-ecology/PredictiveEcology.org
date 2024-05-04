bff392c60803761849cdc7683f426fa553eba5ce
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2024-05-04 00:26:17 -0700

    minor

--- render.R
+++ render.R
@@ -1,8 +1,10 @@
 ## render book
 quarto::quarto_render("training/", as_job = FALSE)
 
+## render website
 quarto::quarto_render(as_job = FALSE)
 
+## now copy book HTMLs to docs/ (always *after* rendering site)
 bookFiles <- list.files("training/_book/", recursive = TRUE, full.names = TRUE)
 bookFilesCopy <- sub("training/_book", "docs/training/_book", bookFiles)
 bookFilesDirs <- dirname(bookFilesCopy)
