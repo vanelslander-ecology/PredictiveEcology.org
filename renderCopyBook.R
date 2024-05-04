6b17ad4b4c97ac85484ed8173382a67ca985785f
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2024-05-04 00:40:15 -0700

    try to setup GHAction

--- renderCopyBook.R
+++ renderCopyBook.R
@@ -1,8 +1,8 @@
-## render book
-quarto::quarto_render("training/", as_job = FALSE)
+## render book -- keep commented out for GHActions
+# quarto::quarto_render("training/", as_job = FALSE)
 
-## render website
-quarto::quarto_render(as_job = FALSE)
+## render website -- keep commented out for GHActions
+# quarto::quarto_render(as_job = FALSE)
 
 ## now copy book HTMLs to docs/ (always *after* rendering site)
 bookFiles <- list.files("training/_book/", recursive = TRUE, full.names = TRUE)
