4d9f3af9b81105b75fdf0dfdd47057205357ff7d
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2024-05-07 09:36:53 -0700

    messaging

--- renderCopyBook.R
+++ renderCopyBook.R
@@ -1,14 +1,16 @@
 ## render book
+message("Rendering book...")
 quarto::quarto_render("training/", as_job = FALSE)
 
 ## render website -- keep commented out for GHActions
+message("Rendering website...")
 quarto::quarto_render(as_job = FALSE)
 
 ## now copy book HTMLs to docs/ (always *after* rendering site)
+message("Copying book files over to website")
 bookFiles <- list.files("training/_book/", recursive = TRUE, full.names = TRUE)
 bookFilesCopy <- sub("training/_book", "docs/training/_book", bookFiles)
 bookFilesDirs <- dirname(bookFilesCopy)
 
 sapply(bookFilesDirs, dir.create, recursive = TRUE, showWarnings = FALSE)
-
 file.copy(bookFiles, bookFilesCopy, overwrite = TRUE)
