019f02b46ce364794e3f7c68d28d812348502bd9
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2024-06-04 15:36:50 -0700

    switch to using pkgList.R

--- training/R/makePkgList.R
+++ training/R/makePkgList.R
@@ -2,12 +2,13 @@
 ## List of packages to pre-install for book
 ## ------------------------------------------------------
 
-## run manually, add libs as need
 pkgList <- Require::pkgSnapshot2(libPaths = c("training/packages/", "training/packages/4.3/",
                                               .libPaths(),
                                               SpaDES.project:::.libPathDefault("castorExample"),
                                               SpaDES.project:::.libPathDefault("integratingSpaDESmodules")))
 pkgList <- c(pkgList, "disk.frame")
 
-write.csv(pkgList, "training/R/pkgList.csv")
-
+cat(paste0("c('",
+           paste(pkgList, collapse = "', \n'"),
+           "')"),
+    file = "training/R/pkgList.R")
